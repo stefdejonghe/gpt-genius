fcf7391e828a3ee902c7f40963e4ba313b763206
Stef De Jonghe <stef.dejonghe@azstlucas.be>
2023-11-29 11:45:01 +0100

    add member profile to sidebar (not working!)

---
+++ components/MemberProfile.jsx
@@ -0,0 +1,15 @@
+import { UserButton, auth, currentUser } from "@clerk/nextjs";
+
+const MemberProfile = async () => {
+  //   const user = await currentUser(); // Uit comments halen op pc thuis
+  //   const { userId } = auth();
+  return (
+    <div className="px-4 flex items-center gap-2">
+      <UserButton afterSignOutUrl="/" />
+      {/* Lijn hieronder uit comments halen op pc thuis */}
+      {/* <p>{user.emailAddresses[0].emailAddress}</p>   */}
+    </div>
+  );
+};
+
+export default MemberProfile;
