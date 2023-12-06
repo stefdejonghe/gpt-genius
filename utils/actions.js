5611d913cc3d008346b5fbf078e43271c53d3793
stefdejonghe <dejonghestef@gmail.com>
2023-12-06 16:48:25 +0100

    add questions and responses to React Query context

--- utils/actions.js
+++ utils/actions.js
@@ -5,16 +5,18 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export const generateChatResponse = async (chatMessage) => {
-  const response = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "you are a helpful assistant" },
-      { role: "user", content: chatMessage },
-    ],
-    model: "gpt-3.5-turbo",
-    temperature: 0,
-  });
-  console.log(response.choices[0].message);
-  console.log(response);
-  return "awesome";
+export const generateChatResponse = async (chatMessages) => {
+  try {
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: "you are a helpful assistant" },
+        ...chatMessages,
+      ],
+      model: "gpt-3.5-turbo",
+      temperature: 0,
+    });
+    return response.choices[0].message;
+  } catch (error) {
+    return null;
+  }
 };
